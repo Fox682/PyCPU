fb53e922da5e813c0a0c49c437a03e47ac40b2f1
Fox682 <digital_ant2@yahoo.com>
2018-09-11 20:13:05 -0600

    v0.9
    Added Memory
    - Added Registers to use memory
    -- ST, LD, LDR, STR
    -- xxR registers are register value relative rather than direct
    Added Stack
    - SP and BP pointers
    Added instructions 
    - ADI
    Modified Load Instructions
    - LD to LDI (LoaD Immediate)

--- compiler.py
+++ compiler.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 from array import *
 #import tempfile
 
+a = 0
+ctr = 0 #counter
 #Vars
 acc = 0
 tgl = 1
@@ -15,16 +17,22 @@ e2 = 0
 #Regs (10)
 ax = '11'
 bx = '12'
-cx = '13'
-dx = '14'
+#cx = '13'
+#dx = '14'
+bp = '15'
+sp = '16'
 
 #Ops (20)
-ld = '21' #Loads val to Reg
-st = '22' #Store Reg to Location in array
+ld = '20' #Load from Memory location
+ldr = '25' #Load using REG reference
+ldi = '21' #Loads Immediate Value to REG
+st = '22' #Store Reg to Location in memory
+str = '26' #Store REG using REG reference
 mov = '23' #Moves Reg to Reg
 cmp = '24' #Compares values to set flag
 
 #Math (30) - Between Regs Only (for now)
+adi = '30'
 add = '31'
 inc = '32'
 sub = '33'
@@ -32,32 +40,72 @@ mul = '34'
 div = '35'
 dec = '36'
 
-#Other
-nop = '99'
+#Stack Ops (40)
+push = '41'
+pop = '42'
+
+#Branching
 jmp = '1000'
 je = '1001'
 jg = '1002'
 jl = '1003'
 
-output = open('outfile2','w')
-outarray = [0 for x in range(0)] #Create array with NOTHING in it
+#Other
+nop = '99'
 
-lines = [line.rstrip('\n') for line in open('infile2')] #creates array with char values from text file
-#print (lines)
+outarray = [0 for x in range(0)] #Create arrays with NOTHING in them
+outarray2 = [0 for x in range(0)]
+
+output = open('outfile','w')
+#with open('outfile2', 'w') as filehandle:
+#    filehandle.writelines("%s\n" % ctr for ctr in outarray)
+
+with open('infile', 'r') as filehandle:
+    input = [ctr.rstrip() for ctr in filehandle.readlines()]
+
+
+def remafterellipsis(intext):
+    where_ellipsis = intext.find(' #')
+    if where_ellipsis == -1:
+        return intext
+    return intext[:where_ellipsis + 0]
+
+#Main Functions here
+
+for line in input:
+    a = remafterellipsis(line)
+    if a.rstrip():
+        outarray2.append(a)
+
+#========== Diagnostic Printing
+#print ("====================")
+#print(">> input <<")
+#print(input)
+#print ("====================")
+#print(">> input2 <<")
+#print(input2)
+#print ("====================")
+#print(">> outarray2 <<")
+#print(outarray2)
+#print ("====================")
+#====== MAIN Part of Program
+#print (a)
+
+
+#a = outarray2
 
 while tgl == 1:
-    a = lines[acc] #Array + value in [slot]
- #   print (a) #print the array value
+    a = outarray2[acc]
     acc+=1 #increment the counter
     if a == '0': #not actual value but ASCII value...
         tgl = 0 #Stops the While loop when a == 0!
 #Split lines to process Commands
-    try: 
+    try:
         s1, s2 = a.split(' ')
         print (s1+" <- s1")
         print (s2+" <- s2")
     except:
-        print ("Exception Processed A")
+        print ("Exception Processed B.")
 ###
     try:
         e1, e2 = s2.split(',')
@@ -66,16 +114,52 @@ while tgl == 1:
     except:
         print ("Exception Processed B.")
 #### Instruction Processing ####
-#### Load AX & BX
-    if s1 == 'ld' and e1 =='ax':
+#### Load Immediate AX & BX
+    if s1 == 'ldi' and e1 == 'ax':
+        outarray.append(ldi+ax+'.'+e2)
+    if s1 == 'ldi' and e1 == 'bx':
+        outarray.append(ldi+bx+'.'+e2)
+
+### Load & Store from Memory
+### Load AX & BX
+    if s1 == 'ld' and e1 == 'ax':
         outarray.append(ld+ax+'.'+e2)
-    if s1 == 'ld' and e1 =='bx':
+    if s1 == 'ld' and e1 == 'bx':
         outarray.append(ld+bx+'.'+e2)
-#### Store AX & BX
-#    if a == 'st':
-#        outarray.append(st)
-#    if a == 'mov':
- #       outarray.append(mov)
+### Store AX & BX
+    if s1 == 'st' and e1 == 'ax':
+        outarray.append(st+ax+'.'+e2)
+    if s1 == 'st' and e1 == 'bx':
+        outarray.append(st+bx+'.'+e2)
+
+### Load REG Refferenced by REG
+### Normally reference are handled like ld bx,[ax]. Feature request
+    if s1 == 'ldr' and e1 == 'ax' and e2 == 'bx':
+        outarray.append(ldr+ax+bx+'.0')
+    if s1 == 'ldr' and e1 == 'bx' and e2 == 'ax':
+        outarray.append(ldr+bx+ax+'.0')
+
+### Store REG by REG
+    if s1 == 'str' and e1 == 'ax' and e2 == 'bx':
+        outarray.append(str+ax+bx+'.0')
+    if s1 == 'str' and e1 == 'bx' and e2 == 'ax':
+        outarray.append(str+bx+ax+'.0')
+### Stack Operations
+### PUSH
+    if s1 == 'push' and s2 == 'ax':
+        outarray.append(push+ax+'.0')
+    if s1 == 'push' and s2 == 'bx':
+        outarray.append(push+bx+'.0')
+#### POP
+    if s1 == 'pop' and s2 == 'ax':
+        outarray.append(pop+ax+'.0')
+    if s1 == 'pop' and s2 == 'bx':
+        outarray.append(pop+bx+'.0')
+
+### Read BP and SP Registers
+
+
+
 #### Maths
 #### ADD AX  <-> BX
     if s1 == 'add' and e1 == 'ax' and e2 == 'bx':
@@ -97,6 +181,17 @@ while tgl == 1:
         outarray.append(div+ax+bx+'.0')
     if s1 == 'div' and e1 == 'bx' and e2 =='ax':
         outarray.append(div+bx+ax+'.0')
+
+#### ADD Immediate
+    if s1 == 'adi' and e1 == 'ax' and e2 != 'bx':
+#    if s1 == 'adi' and e1 == 'ax' and int(e2) > 0:
+        outarray.append(adi+ax+'.'+e2)
+    if s1 == 'adi' and e1 == 'bx' and e2 !='ax':
+#    if s1 == 'adi' and e1 == 'bx' and int(e2) > 0:
+        outarray.append(adi+bx+'.'+e2)
+
+#### Other Operations
+
 #### INC
     if s1 == 'inc' and s2 == 'ax':
         outarray.append(inc+ax+'.0')
@@ -107,36 +202,40 @@ while tgl == 1:
         outarray.append(dec+ax+'.0')
     if s1 == 'dec' and s2 == 'bx':
         outarray.append(dec+bx+'.0')
+
+#### Branching Instructions
 #### JMP
     if s1 == 'jmp':
-        outarray.append(jmp+'.'+ str(s2) )  
+        outarray.append(jmp+'.'+s2) #Instead of str(s2)
 #### Conditional JMPs
 # JE - Jump if Equal
     if s1 == 'je':
-        outarray.append(je+'.'+ str(s2) )    
+        outarray.append(je+'.'+s2)
 #JG - Jump if Greater
     if s1 == 'jg':
-        outarray.append(jg+'.'+ str(s2) )
+        outarray.append(jg+'.'+s2)
 #JL - Jump if Less than
     if s1 == 'jl':
-        outarray.append(jl+'.'+ str(s2) )
+        outarray.append(jl+'.'+s2)
 ##
-### NOP doesn't work... try again
+### NOP
     if s1 == 'nop':
         outarray.append(nop +'.0')
-### CMP 
+### CMP
     if s1 == 'cmp' and e1 == 'ax' and e2 == 'bx':
         outarray.append(cmp+ax+bx+'.0')
     if s1 == 'cmp' and e1 == 'bx' and e2 == 'ax':
-        outarray.append(cmp+bx+ax+'.0')        
-#### 
-#### 
+        outarray.append(cmp+bx+ax+'.0')
+####
+####
 
 
 #### Other
 outarray.pop() #Fixes duplicate entry at end of array (bug)
 outarray.append('0') #Tells the CPU & Compiler to stop
-
+print ("====================")
+print(">> outrray <<")
+print(outarray)
 ### 2nd Pass for JMPs to have Labels
 ### Ensure Above works before doing this...
 #while tgl == 1:
@@ -145,27 +244,17 @@ outarray.append('0') #Tells the CPU & Compiler to stop
 #    acc+=1 #increment the counter
 #    if a == '0': #not actual value but ASCII value...
 #        tgl = 0 #Stops the While loop when a == 0!
-# 
+#
 # Need to count the distance between the Labels
 #
 #
 # Iterate over the Array
 #Scan for Labels [Pick a format for them]
 # Use the Split above to split the jmp and the Value
-# if  a = "jmp" 
+# if  a = "jmp"
 # then Use s2 to track the labels
 
 
-
-
-#print(outarray)
-###
-# Write the array of numbers to the file
-strarray = list(map(str, outarray)) #Makes a list
-#print (strarray)
-
 #Writes the "list" to the file!
-for item in strarray: 
+for item in outarray:
   output.write("%s\n" % item)
-
-
--- cpu.py
+++ cpu.py
@@ -12,50 +12,127 @@ s2 = 0
 #Opcodes
 ax = 0
 bx = 0
-cx = 0
-dx = 0
+#cx = 0
+#dx = 0
 cmp = 0
 #Counters
 pc = 0
 clk = 0
 on = 1
-#####
+#########
+#Memory Variable, EDIT MEMORY SIZE HERE
+m = 25
+#Stack init
+bp = m-1
+sp = m-1
 ###
 flag = 0 #Use an int use str(flag) to convert it to a string
 #Flag for CMP  values
-# 1 "==", 2 "!=", 3 ">", 4 "<"
 
+#Local Memory for CPU, set for as much as needed
+mem = [0 for x in range(m)]
 
-lines = [line.rstrip('\n') for line in open('outfile2')] #creates array with char values from text file
-#print (lines)
-#First Line is 0 not 1
+lines = [line.rstrip('\n') for line in open('outfile')] #creates array with char values from text file
 
 while on == 1:
-#    clk+=1   
-#    pc  +=1        
     while tgl == 1:
-        sleep(0.10) #Sleep 0.10 == 10 Cycles per Second, 0.001 = 1 ms (1khz), Sleep 0.0001 = 0.1ms (10khz)
+        sleep(0.1) #Sleep 0.10 == 10 Cycles per Second, 0.001 = 1 ms (1khz), Sleep 0.0001 = 0.1ms (10khz)
         a = lines[acc] #Array + value in [slot]
-#        print (lines) #print the array value
         acc = acc + 1
         if a == '0':
             tgl = 0 #mod
         try:
             e1, e2 = a.split(".")
-#            print (e1+" <- e1")
-#            print (e2+" <- e2")
         except:   #This will Exit the Program if it gets something unknown
             print("====================")
             print("End of Program or")
-            print ("Unknown Opcode Below") 
+            print ("Unknown Opcode Below")
             on = 0 #on/off for CPU
-
-### Possible Optimization to use If Else rather than consecutive IF's
-
-#####
+########
         if e1 == "99": #NOP
             pc = pc + 1
             clk = clk + 1
+##### Load Immediate
+        if e1 == "2111": #LDI AX,#
+            ax = int(e2)
+            pc = pc + 1
+            clk =clk + 1
+        if e1 == "2112": #LDI BX,#
+            bx = int(e2)
+            pc = pc + 1
+            clk += 1
+### Load and Store from Memory
+### Load AX & BX
+        if e1 == "2011": #LD AX,[#]
+            e2 = int(e2)
+            ax = mem[e2]
+            pc = pc + 1
+            clk =clk + 1
+        if e1 == "2012": #LD BX,[#]
+            e2 = int(e2)
+            bx = mem[e2]
+            pc = pc + 1
+            clk += 1
+### Store AX & BX
+        if e1 == "2211": #ST AX,[#]
+            e2 = int(e2)
+            mem[e2] = ax
+            pc = pc + 1
+            clk =clk + 1
+        if e1 == "2212": #ST BX,[#]
+            e2 = int(e2)
+            mem[e2] = bx
+            pc = pc + 1
+            clk += 1
+### Load REG by REG
+        if e1 == "251112": #LDR AX,BX
+            #e2 = int(e2)
+            ax = mem[bx]
+            pc = pc + 1
+            clk =clk + 1
+        if e1 == "251211": #LDR BX,AX
+            #e2 = int(e2)
+            bx = mem[ax]
+            pc = pc + 1
+            clk += 1
+### Store REG by REG
+        if e1 == "261112": #STR AX,BX
+            e2 = int(e2)
+            mem[ax] = bx
+            pc = pc + 1
+            clk =clk + 1
+        if e1 == "261211": #STR BX,AX
+            e2 = int(e2)
+            mem[bx] = ax
+            pc = pc + 1
+            clk += 1
+### PUSH & POP
+        if e1 == "4111": #PUSH AX
+            mem[sp] = ax
+            pc = pc + 1
+            clk = clk + 1
+            sp = sp - 1
+        if e1 == "4112": #PUSH BX
+            mem[sp] = bx
+            pc = pc + 1
+            clk = clk + 1
+            sp = sp - 1
+### POP
+        if e1 == "4211": #POP AX
+            sp = sp + 1
+            ax = mem[sp]
+            pc = pc + 1
+            clk = clk + 1
+        if e1 == "4212": #POP BX
+            sp = sp + 1
+            bx = mem[sp]
+            pc = pc + 1
+            clk = clk + 1
+
+### Modify SP & BP with REG
+
+
+#### Math Operations Go Here:
 ##### Increment
         if e1 == "3211": #INC AX
             ax = ax + 1
@@ -74,15 +151,6 @@ while on == 1:
             bx = bx - 1
             pc = pc + 1
             clk = clk + 1
-##### Load Immediate
-        if e1 == "2111": #LD AX,#
-            ax = int(e2)
-            pc = pc + 1
-            clk =clk + 1
-        if e1 == "2112": #LD BX,#
-            bx = int(e2)
-            pc = pc + 1
-            clk += 1
 ##### ADD REG
         if e1 == "311112": #ADD AX,BX
             ax = int(ax)+int(bx)
@@ -119,16 +187,17 @@ while on == 1:
             bx = int(bx)/int(ax)
             pc = pc + 1
             clk = clk + 1
-##### ADD IMMEDIATE VALUE - AX <-> BX
-##### Works, not sure if I want to implement this...
-#        if e1 == "3111": #ADD AX,IMM
-#            ax = int(bx)+int(e2)
-#            pc = pc + 1
-#            clk = clk + 1
-#        if e1 == "3112": #ADD BX,IMM
-#            bx = int(bx)+int(e2)
-#            pc = pc + 1
-#            clk = clk + 1
+##### ADD IMMEDIATE VALUE
+        if e1 == "3011": #ADD AX,IMM
+            ax = int(ax)+int(e2)
+            pc = pc + 1
+            clk = clk + 1
+        if e1 == "3012": #ADD BX,IMM
+            bx = int(bx)+int(e2)
+            pc = pc + 1
+            clk = clk + 1
+
+#### Branching Instructions Go Here:
 ##### JMP Immediate
         if e1 == "1000": #JMP,PC
             x = int(e2)
@@ -136,8 +205,7 @@ while on == 1:
             pc = acc
             clk = clk + 1
             e1 = e2 = 0  #Clear these if you do!
-#JMP Branching
-##JE - JMP if ==
+##JE - JMP ==
         if e1 == '1001' and flag == 1:
             x = int(e2)
             acc = acc+x
@@ -149,7 +217,7 @@ while on == 1:
                 acc = acc + 1
                 pc = acc
                 e1 = e2 = 0
-##JG - JMP if >
+##JG - JMP >
         if e1 == '1002' and flag == 12:
             x = int(e2)
             acc = acc+x
@@ -193,17 +261,16 @@ while on == 1:
             clk = clk + 1
             e1 = e2 = 0
             if e1 != '1002' and flag != 23:
-                clk = clk +1
+                clk = clk + 1
                 acc = acc + 1
                 pc = acc
                 e1 = e2 = 0
-            
 ##### CMP ==
         if e1 == "241112" or e1 == "241211" and int(ax) == int(bx):
             flag = 1
             pc = pc + 1
             clk = clk + 1
-##### CMP > & < (AX vs BX)         
+##### CMP > & < (AX vs BX)
         if e1 == "241112" and int(ax) > int(bx):
             flag = 12
             pc = pc + 1
@@ -221,17 +288,18 @@ while on == 1:
             flag = 23
             pc = pc + 1
             clk = clk + 1
-#####
+############################################################
 
-
-##### Status Print - Comment out for PERFORMANCE!     
+##### Status Print - Comment out for PERFORMANCE!
         print("=========================")
         print("AX = "+ str(ax), "BX = "+ str(bx))
+        print("SP = "+ str(sp), "BP = "+ str(bp))
         print("CLK = "+str(clk), " PC = "+ str(pc))
         print("OpCode = "+str(e1) + " Param = " +str(e2))
-        print("Array = " + str(acc) + " CMP F = " + str(flag))  
+        print("Array = " + str(acc) + " CMP F = " + str(flag))
+        print("=========================")
+        print(mem)
 ## Don't remove below this line
-        e1 = e2 = 0 # Reset this to fix Execution Bug (extra clock at end of program)
-    
+        e1 = e2 = s1 = s2 = 0 # Reset this to fix Execution Bug (extra clock at end of program)
+
 ###
-    
